8798dcd31bc36a00b988ae2106058923daf3a972
Sandro Muniz <sandrojam@hotmail.com>
2024-09-04 14:36:39 -0300

    Criando os repositórios Marca e Produto
    Serão responsáveis pela persistência no Banco de Dados Base no MySQL.

---
+++ src/main/java/br/com/evolution/apirelacionamentoentreentidades/repository/ProdutoRespositorio.java
@@ -0,0 +1,11 @@
+package br.com.evolution.apirelacionamentoentreentidades.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.evolution.apirelacionamentoentreentidades.model.Produto;
+
+@Repository
+public interface ProdutoRespositorio extends CrudRepository<Produto, Long> {
+    
+}
