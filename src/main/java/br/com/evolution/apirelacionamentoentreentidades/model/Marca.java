80818519ef076d32c164655e13c22d53a7c4ba83
Sandro Muniz <sandrojam@hotmail.com>
2024-09-04 14:10:33 -0300

    Implementando o relacionamento ManyToMany
    Criar vínculo entre os modelos Marca e Produto, duas novas tabelas serão geradas marcas_produtos e produtos_marcas,  mas isso será resolvido.

--- src/main/java/br/com/evolution/apirelacionamentoentreentidades/model/Marca.java
+++ src/main/java/br/com/evolution/apirelacionamentoentreentidades/model/Marca.java
@@ -1,9 +1,13 @@
 package br.com.evolution.apirelacionamentoentreentidades.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,5 +23,8 @@ public class Marca {
     private long codigo;
     
     private String marca;
+
+    @ManyToMany
+    private List<Produto> produtos = new ArrayList<>();
     
 }
--- src/main/java/br/com/evolution/apirelacionamentoentreentidades/model/Produto.java
+++ src/main/java/br/com/evolution/apirelacionamentoentreentidades/model/Produto.java
@@ -1,9 +1,13 @@
 package br.com.evolution.apirelacionamentoentreentidades.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,5 +25,8 @@ public class Produto {
     private String nome;
 
     private double preco;
+
+    @ManyToMany
+    private List<Marca> marcas = new ArrayList<>();
     
 }
